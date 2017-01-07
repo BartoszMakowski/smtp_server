e29e25615171d62c79b67fc0d87feadde5a64102
BartoszMakowski <baboshpl@gmail.com>
2017-01-07 21:52:39 +0100

    added readFrom/To() functions

--- main.c
+++ main.c
@@ -14,14 +14,59 @@ int readEhlo(int cSocket){
 	int n;
 	int count;
 	count = 0;
-	while (strncmp (line, "EHLO ", 5) != 0 && strncmp (line, "ehlo ", 5) != 0 && strncmp (line, "quit", 4) != 0){
-		if (count){
+	do
+	{
+		if (count++){
 			write(cSocket, "500 unrecognized command\r\n", 27);
+			if (count == 4){
+				return(1);
+			}
 		}
 		read(cSocket, line, 32);
-		count += 1;
 	}
-	write(cSocket, "250 inf122518_smtp_server: Hello\r\n", 40);
+	while (strncmp (line, "EHLO ", 5) != 0 && strncmp (line, "ehlo ", 5) != 0 && strncmp (line, "quit", 4) != 0);
+	write(cSocket, "250 inf122518_smtp_server: Hello\r\n", 34);
+	return(0);
+}
+
+int readFrom(int cSocket){
+	char line[64];
+	int n;
+	int count;
+	count = 0;
+	do
+	{
+		if (count++){
+			write(cSocket, "500 unrecognized command\r\n", 27);
+			if (count == 4){
+				return(1);
+			}
+		}
+		read(cSocket, line, 32);
+	}
+	while (strncmp (line, "mail from: ", 11) != 0 && strncmp (line, "MAIL FROM: ", 11) != 0 && strncmp (line, "quit", 4) != 0);
+	write(cSocket, "250 OK\r\n", 8);
+	return(0);
+}
+
+int readTo(int cSocket){
+	char line[64];
+	int n;
+	int count;
+	count = 0;
+	do
+	{
+		if (count++){
+			write(cSocket, "500 unrecognized command\r\n", 27);
+			if (count == 4){
+				return(1);
+			}
+		}
+		read(cSocket, line, 32);
+	}
+	while (strncmp (line, "rcpt to: ", 9) != 0 && strncmp (line, "RCPT TO: ", 9) != 0 && strncmp (line, "quit", 4) != 0);
+	write(cSocket, "250 OK\r\n", 8);
+	return(0);
 }
 
 int main(int argc, char* argv[])
@@ -85,6 +130,8 @@ int main(int argc, char* argv[])
 		nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
 		write(nClientSocket, "220 inf122518_smtp_server\r\n", 28);
 		readEhlo(nClientSocket);
+		readFrom(nClientSocket);
+		readTo(nClientSocket);
 		close(nClientSocket); 
 	}
 
