d394d5496dd6f87227ac5d60ca74f5605c8badd4
BartoszMakowski <baboshpl@gmail.com>
2017-01-11 23:20:05 +0100

    added senderFromMail() function

--- main.c
+++ main.c
@@ -12,23 +12,44 @@
 #define BUF_SIZE 1024
 #define BACKLOG 10
 
+int senderFromMail(char *line, char **sender){
+	*sender = strtok(line, "<");
+	*sender = strtok(NULL, ">");
+//	TODO: mail syntax validation
+//	fprintf(stdout, "sender: %s\n", *sender);
+	return(0);
+}
+
 int readFrom(int cSocket){
-	char line[64];
+	char line[128];
+	char *sender;
 	int count;
 	count = 0;
 	do
 	{
-		if (count++){
-			write(cSocket, "500 unrecognized command\r\n", 27);
-			if (count == 4){
-				return(1);
+		read(cSocket, line, 32);
+		if (strncmp (line, "MAIL FROM:", 10) == 0 || strncmp (line, "mail from:", 10) == 0){
+			// TODO
+			if (senderFromMail(line, &sender) == 0){
+				fprintf(stdout, "sender: %s\n", sender);
+				write(cSocket, "250 OK\r\n", 8);
+				return(0);
+			}
+			else{
+				return(-1);
 			}
 		}
-		read(cSocket, line, 32);
-	}
-	while (strncmp (line, "mail from: ", 11) != 0 && strncmp (line, "MAIL FROM: ", 11) != 0 && strncmp (line, "quit", 4) != 0);
-	write(cSocket, "250 OK\r\n", 8);
-	return(0);
+		else if (strncmp (line, "quit\r\n", 6) == 0 || strncmp (line, "QUIT\r\n", 6) == 0){
+			//TODO
+			exit(-1);
+		}
+		else{
+			//TODO
+			write(cSocket, "500 unrecognized command\r\n", 27);
+			count++;
+		}
+	} while ( count < 3);
+	return(-1);
 }
 
 int readTo(int cSocket){
