8482e171ccec4fd46b718a2b8151e39ac046260b
BartoszMakowski <baboshpl@gmail.com>
2017-01-12 23:28:06 +0100

    added readData() function

--- main.c
+++ main.c
@@ -13,6 +13,65 @@
 #define BUF_SIZE 1024
 #define BACKLOG 10
 
+int readDataCmd(int cSocket){
+	char line[128];
+	char *sender;
+	char **data;
+	int count;
+	count = 0;
+	do
+	{
+		read(cSocket, line, 128);
+		if (strncmp (line, "DATA\r\n:", 6) == 0 || strncmp (line, "data\r\n", 6) == 0){
+			write(cSocket, "354 Send message\r\n", 19);
+			readData(cSocket, &data);
+			return(0);			
+		}
+		else if (strncmp (line, "quit\r\n", 6) == 0 || strncmp (line, "QUIT\r\n", 6) == 0){
+			//TODO
+			exit(-1);
+		}
+		else{
+			//TODO
+			write(cSocket, "500 unrecognized command\r\n", 27);
+			count++;
+		}
+	} while ( count < 3);
+	return(-1);
+}
+
+int readData(int cSocket, char** data){
+	char line[128];
+	char *sender;
+	int n;
+	int count;
+	int end;
+	end = 0;
+	count = 0;
+	*data = malloc(1);
+	do
+	{
+		n = read(cSocket, line, 1024);
+		if (strncmp (line, ".\r\n", 3) == 0){
+			return(0);
+		}
+		else if (strncmp (line, "quit\r\n", 6) == 0 || strncmp (line, "QUIT\r\n", 6) == 0){
+			//TODO
+			exit(-1);
+		}
+		else{
+			fprintf(stdout, "read %d chars\n", n);
+			realloc(*data, strlen(*data) + strlen(line) + 1);
+			strcat(*data, line);
+			fprintf(stdout, "DATA:\n%s", *data);
+			//TODO
+			//write(cSocket, "read %i chars", n, 27);
+			count++;
+		}
+	} while ( !end && count < 10);
+	return(-1);
+}
+
 int main(int argc, char* argv[])
 {
 	int nSocket;
@@ -76,6 +135,7 @@ int main(int argc, char* argv[])
 		clientInit(nClientSocket);
 		readFrom(nClientSocket);
 		readTo(nClientSocket);
+		readDataCmd(nClientSocket);
 		close(nClientSocket); 
 	}
 
