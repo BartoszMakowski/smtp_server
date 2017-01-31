b193e8c6bf713362532a01c0c759a8442fda9cfc
BartoszMakowski <baboshpl@gmail.com>
2017-01-31 01:53:36 +0100

    added client (draft)

--- client.c
+++ client.c
@@ -11,22 +11,57 @@ int createCon(char *server_name, int port){
 	memcpy(&server_address.sin_addr.s_addr, server_host_entity->h_addr, server_host_entity->h_length);
 	server_address.sin_port = htons(port);
 	int nConnect = connect(desc, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
-	fprintf(stdout, "STATUS: %d", nConnect);
+//	fprintf(stdout, "STATUS: %d", nConnect);
 	return(desc);
 }
 
 int sendMail(struct sMail *mail, int desc){
 	char *message;
 	char inMessage[1000];
-	fprintf(stdout, "MALLOC\n");
-	message = malloc(15 + strlen((*mail).sender));
+//	fprintf(stdout, "MALLOC\n");
+	message = malloc((15 + strlen((*mail).sender)) * sizeof(char));
+
 	strcat(message, "mail from: <");
 	strcat(message, (*mail).sender);
 	strcat(message, ">\r\n");
-	fprintf(stdout, "WRITE\n");
+
+//	fprintf(stdout, "WRITE\n");
+	read(desc, inMessage, 1000);
+//	fprintf(stdout, "READ: %s\nWRITE: %s", inMessage, message);
+	write(desc, message, strlen(message));
+	message = '\0';
+
+	message = realloc(message, (13 + strlen((*mail).recipients))*sizeof(char));
+	strcat(message, "rcpt to: <");
+	strcat(message, (*mail).recipients);
+	strcat(message, ">\r\n");
+
+	read(desc, inMessage, 1000);
+//	fprintf(stdout, "READ: %s\nWRITE: %s", inMessage, message);
+	write(desc, message, strlen(message));
+
+//	free(message);
+	message = realloc(message, 6 * sizeof(char));
+	strcpy(message, "data\r\n");
+	read(desc, inMessage, 1000);
+//	fprintf(stdout, "READ: %s\nWRITE: %s", inMessage, message);
+	write(desc, message, strlen(message));
+
+	strcat(message, (*mail).data);
+	read(desc, inMessage, 1000);
+//	fprintf(stdout, "READ: %s\nWRITE: %s", inMessage, message);
+	write(desc, (*mail).data, strlen((*mail).data));
+	
+
+	strcpy(message, "quit\r\n");
 	read(desc, inMessage, 1000);
 	fprintf(stdout, "READ: %s\nWRITE: %s", inMessage, message);
 	write(desc, message, strlen(message));
+	
+	free(message);
+	read(desc, inMessage, 1000);
+	fprintf(stdout, "READ: %s", inMessage);
+
 	close(desc);
 	return(0);
 }
--- main.c
+++ main.c
@@ -163,56 +163,6 @@ void *threadBehavior(void *tData){
 	pthread_exit(NULL);
 }
 
-int quitSuccess(int cSocket){
-	write(cSocket, "inf122518_smtp_server: Service closing transmission channel\r\n", 61);
-	return(0);
-}
-
-int readDataCmd(int cSocket, char *line, struct sMail *mail){
-	char *data;
-	write(cSocket, "354 Send message\r\n", 19);
-	fprintf(stdout, "354 send message\r\n");
-	if(readData(cSocket, &data) == 0){
-		(*mail).data = malloc(sizeof(char) * strlen(data));
-		strcpy((*mail).data, data);
-//		fprintf(stdout, "zwracam zero\r\n");
-		return(0);
-	}
-	else{
-		return(-1);
-	}
-}
-
-int readData(int cSocket, char** data){
-	char line[1000];
-	char *sender;
-	int n;
-	int end;
-	int status;
-	end = 0;
-	*data = malloc(0);
-	do
-	{
-		n = read(cSocket, line, 1000);
-//		fprintf(stdout, "READ LINE:\n%s\r\n$$$", line);
-		line[n]='\0';
-			if (DEBUG)
-			{
-				fprintf(stdout, "read %d chars\n", n);
-			}
-			*data = realloc(*data, strlen(*data) + n-2);
-			strcat(*data, line);
-			if (n >= 3 && line[n-3] == '.' && line[n-2] == '\r' && line[n-1] == '\n')
-			{
-				write(cSocket, "250 OK\r\n", 8);
-				status = 0;
-				end = 1;
-			}
-	} while ( !end );
-//	fprintf(stdout, "KONIEC\r\n");
-	return(status);
-}
-
 int main(int argc, char* argv[])
 {
 	int nSocket;
